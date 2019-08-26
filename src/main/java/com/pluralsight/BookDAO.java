1ef4f9da892c134a5974bd17cba6105b8816dfc4
Shiro Toran <toran.shiro@outlook.com>
2019-08-26 17:08:23 +0900

    Update BookDAO.java

--- src/main/java/com/pluralsight/BookDAO.java
+++ src/main/java/com/pluralsight/BookDAO.java
@@ -107,8 +107,8 @@ public class BookDAO {
     }
     
     public void updateBook(Book book) {
-    	String sql = "UPDATE book SET title = ?, author = ?, price = ?" + 
-    				 "WHERE id =?";
+    	String sql = "UPDATE book SET title = ?, author = ?, price = ? WHERE id =?";
+    	
     	try {
 			PreparedStatement statement = jdbcConnection.prepareStatement(sql);
 			statement.setString(1, book.getTitle());
